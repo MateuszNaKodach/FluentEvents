98d19eb609736aee4928c3eef39de7e750f05384
luca-esse <luca-esse@users.noreply.github.com>
2019-01-28 17:28:59 +0100

    Add documentation

--- src/FluentEvents/Config/PipelinesBuilder.cs
+++ src/FluentEvents/Config/PipelinesBuilder.cs
@@ -17,6 +17,15 @@ namespace FluentEvents.Config
             m_SourceModelsService = sourceModelsService;
         }
 
+        /// <summary>
+        /// Registers an entity and an event as part of the model and returns an object that can be used to
+        /// configure how the event is handled. This method can be called multiple times for the same event to
+        /// configure multiple pipelines.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the event source.</typeparam>
+        /// <typeparam name="TEventArgs">The type of the event args.</typeparam>
+        /// <param name="eventFieldName">The name of the event field.</param>
+        /// <returns>The configuration object for the specified event.</returns>
         public EventConfigurator<TSource, TEventArgs> Event<TSource, TEventArgs>(
             string eventFieldName
         )
