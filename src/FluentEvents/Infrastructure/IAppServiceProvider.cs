dbe28047556e9904c2bd1f2ec4cbf4c4fc920794
luca-esse <luca-esse@users.noreply.github.com>
2019-02-25 09:43:33 +0100

    Add IAppServiceProvider xml summary

--- src/FluentEvents/Infrastructure/IAppServiceProvider.cs
+++ src/FluentEvents/Infrastructure/IAppServiceProvider.cs
@@ -2,6 +2,9 @@
 
 namespace FluentEvents.Infrastructure
 {
+    /// <summary>
+    ///     Represents the application service provider.
+    /// </summary>
     public interface IAppServiceProvider : IServiceProvider
     {
     }
