af72a0f6dee8f0a4b8276bf930fcdcace99c45d7
luca-esse <luca-esse@users.noreply.github.com>
2019-02-28 11:42:07 +0100

    Add xml docs

--- src/FluentEvents/Routing/IAttachingInterceptor.cs
+++ src/FluentEvents/Routing/IAttachingInterceptor.cs
@@ -1,7 +1,16 @@
 ﻿namespace FluentEvents.Routing
 {
+    /// <summary>
+    ///     This interface can be implemented to intercept every attaching made to the <see cref="EventsContext"/>.
+    /// </summary>
     public interface IAttachingInterceptor
     {
+        /// <summary>
+        ///     This method can be used to handle complex attaching situations (For example attaching to the entities of an ORM).
+        /// </summary>
+        /// <param name="attachingService">The attaching service that invokes the interceptor.</param>
+        /// <param name="source">The events source being attached.</param>
+        /// <param name="eventsScope">The current <see cref="EventsScope"/></param>
         void OnAttaching(IAttachingService attachingService, object source, EventsScope eventsScope);
     }
 }
