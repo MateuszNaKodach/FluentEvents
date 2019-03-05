33ec69a307eb857a9ede799ea1a79d3c70201a4e
luca-esse <luca-esse@users.noreply.github.com>
2019-03-05 12:56:19 +0100

    Add FluentEvents.Azure.ServiceBus unit tests

---
+++ src/FluentEvents.Azure.ServiceBus.Tests/TopicEventReceiverConfigTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentEvents.Azure.ServiceBus.Receiving;
+using NUnit.Framework;
+
+namespace FluentEvents.Azure.ServiceBus.Tests
+{
+    [TestFixture]
+    public class TopicEventReceiverConfigTests
+    {
+        private const string InvalidConnectionString = "InvalidConnectionString";
+        private const string ValidConnectionString = "Endpoint=sb://sbdomain.net/;SharedAccessKeyName=read;SharedAccessKey=123;EntityPath=123";
+
+        private TopicEventReceiverConfig m_TopicEventReceiverConfig;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_TopicEventReceiverConfig = new TopicEventReceiverConfig();
+        }
+
+        [Test]
+        public void ManagementConnectionString_WhenConnectionStringIsInvalid_ShouldThrow()
+        {
+            Assert.That(() =>
+            {
+                m_TopicEventReceiverConfig.ManagementConnectionString = InvalidConnectionString;
+            }, Throws.TypeOf<InvalidConnectionStringException>());
+        }
+
+        [Test]
+        public void ManagementConnectionString_WhenConnectionStringIsValid_ShouldSet()
+        {
+            m_TopicEventReceiverConfig.ManagementConnectionString = ValidConnectionString;
+
+            Assert.That(
+                m_TopicEventReceiverConfig,
+                Has
+                    .Property(nameof(TopicEventReceiverConfig.ManagementConnectionString))
+                    .EqualTo(ValidConnectionString)
+            );
+        }
+
+        [Test]
+        public void ReceiveConnectionString_WhenConnectionStringIsInvalid_ShouldThrow()
+        {
+            Assert.That(() =>
+            {
+                m_TopicEventReceiverConfig.ReceiveConnectionString = InvalidConnectionString;
+            }, Throws.TypeOf<InvalidConnectionStringException>());
+        }
+
+        [Test]
+        public void ReceiveConnectionString_WhenConnectionStringIsValid_ShouldSet()
+        {
+            m_TopicEventReceiverConfig.ReceiveConnectionString = ValidConnectionString;
+
+            Assert.That(
+                m_TopicEventReceiverConfig,
+                Has
+                    .Property(nameof(TopicEventReceiverConfig.ReceiveConnectionString))
+                    .EqualTo(ValidConnectionString)
+            );
+        }
+    }
+}
---
+++ src/FluentEvents.Azure.ServiceBus.Tests/TopicEventSenderConfigTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentEvents.Azure.ServiceBus.Sending;
+using NUnit.Framework;
+
+namespace FluentEvents.Azure.ServiceBus.Tests
+{
+    [TestFixture]
+    public class TopicEventSenderConfigTests
+    {
+        private const string InvalidConnectionString = "InvalidConnectionString";
+        private const string ValidConnectionString = "Endpoint=sb://sbdomain.net/;SharedAccessKeyName=read;SharedAccessKey=123;EntityPath=123";
+
+        private TopicEventSenderConfig m_TopicEventSenderConfig;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_TopicEventSenderConfig = new TopicEventSenderConfig();
+        }
+
+        [Test]
+        public void ConnectionString_WhenConnectionStringIsInvalid_ShouldThrow()
+        {
+            Assert.That(() =>
+            {
+                m_TopicEventSenderConfig.ConnectionString = InvalidConnectionString;
+            }, Throws.TypeOf<InvalidConnectionStringException>());
+        }
+
+        [Test]
+        public void ConnectionString_WhenConnectionStringIsValid_ShouldSet()
+        {
+            m_TopicEventSenderConfig.ConnectionString = ValidConnectionString;
+
+            Assert.That(
+                m_TopicEventSenderConfig,
+                Has
+                    .Property(nameof(TopicEventSenderConfig.ConnectionString))
+                    .EqualTo(ValidConnectionString)
+            );
+        }
+    }
+}
