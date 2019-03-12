fcd6691a9234c3a111043ed193297ab218bf559b
luca-esse <luca-esse@users.noreply.github.com>
2019-03-12 12:20:04 +0100

    Add UrlProvider tests

---
+++ src/FluentEvents.Azure.SignalR.UnitTests/Clients/UrlProviderTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentEvents.Azure.SignalR.Client;
+using NUnit.Framework;
+
+namespace FluentEvents.Azure.SignalR.UnitTests.Clients
+{
+    [TestFixture]
+    public class UrlProviderTests
+    {
+        private const string Endpoint = "http://endpoint";
+        private const string HubName = nameof(HubName);
+        private const string ReceiverId = nameof(ReceiverId);
+
+        private UrlProvider m_UrlProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_UrlProvider = new UrlProvider();
+        }
+
+        [Test]
+        public void GetUrl_ShouldTrimEndpointSlash()
+        {
+            var url = m_UrlProvider.GetUrl(Endpoint + "/", PublicationMethod.Broadcast, HubName, null);
+
+            Assert.That(url, Is.EqualTo(Endpoint + $"/api/v1/hubs/{HubName.ToLower()}"));
+        }
+
+        [Test]
+        public void GetUrl_WithPublicationTypeBroadcast_ShouldReturnCorrectUrl()
+        {
+            var url = m_UrlProvider.GetUrl(Endpoint, PublicationMethod.Broadcast, HubName, null);
+            
+            Assert.That(url, Is.EqualTo(Endpoint + $"/api/v1/hubs/{HubName.ToLower()}"));
+        }
+
+        [Test]
+        public void GetUrl_WithPublicationTypeUser_ShouldReturnCorrectUrl()
+        {
+            var url = m_UrlProvider.GetUrl(Endpoint, PublicationMethod.User, HubName, ReceiverId);
+
+            Assert.That(url, Is.EqualTo(Endpoint + $"/api/v1/hubs/{HubName.ToLower()}/user/{ReceiverId}"));
+        }
+
+        [Test]
+        public void GetUrl_WithPublicationTypeGroup_ShouldReturnCorrectUrl()
+        {
+            var url = m_UrlProvider.GetUrl(Endpoint, PublicationMethod.Group, HubName, ReceiverId);
+
+            Assert.That(url, Is.EqualTo(Endpoint + $"/api/v1/hubs/{HubName.ToLower()}/group/{ReceiverId}"));
+        }
+    }
+}
--- src/FluentEvents.Azure.SignalR/Client/UrlProvider.cs
+++ src/FluentEvents.Azure.SignalR/Client/UrlProvider.cs
@@ -28,6 +28,6 @@ namespace FluentEvents.Azure.SignalR.Client
         private string GetSendToUserUrl(string endpoint, string hubName, string userId) => $"{GetBaseUrl(endpoint, hubName)}/user/{userId}";
         private string GetSendToGroupUrl(string endpoint, string hubName, string group) => $"{GetBaseUrl(endpoint, hubName)}/group/{group}";
         private string GetBroadcastUrl(string endpoint, string hubName) => $"{GetBaseUrl(endpoint, hubName)}";
-        private string GetBaseUrl(string endpoint, string hubName) => $"{endpoint}/api/v1/hubs/{hubName.ToLower()}";
+        private string GetBaseUrl(string endpoint, string hubName) => $"{endpoint.TrimEnd('/')}/api/v1/hubs/{hubName.ToLower()}";
     }
 }
