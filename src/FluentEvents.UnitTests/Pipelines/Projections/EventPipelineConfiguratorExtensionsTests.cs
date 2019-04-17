2a646ac1c6b8e84087d5ca7d99b442dc316e0868
luca-esse <luca-esse@users.noreply.github.com>
2019-04-18 00:42:32 +0200

    Add event selector for projections

--- src/FluentEvents.UnitTests/Pipelines/Projections/EventPipelineConfiguratorExtensionsTests.cs
+++ src/FluentEvents.UnitTests/Pipelines/Projections/EventPipelineConfiguratorExtensionsTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using FluentEvents.Config;
 using FluentEvents.Model;
 using FluentEvents.Infrastructure;
 using FluentEvents.Pipelines;
 using FluentEvents.Pipelines.Projections;
+using FluentEvents.Utils;
 using Moq;
 using NUnit.Framework;
 
@@ -14,7 +17,9 @@ namespace FluentEvents.UnitTests.Pipelines.Projections
     {
         private Mock<IServiceProvider> m_ServiceProviderMock;
         private Mock<ISourceModelsService> m_SourceModelsServiceMock;
+        private Mock<IEventSelectionService> m_EventSelectionServiceMock;
         private SourceModel m_SourceModel;
+        private SourceModel m_ProjectedSourceModel;
         private SourceModelEventField m_SourceModelEventField;
         private Mock<IPipeline> m_PipelineMock;
         private EventPipelineConfigurator<TestSource, TestEventArgs> m_EventPipelineConfigurator;
@@ -24,7 +29,9 @@ namespace FluentEvents.UnitTests.Pipelines.Projections
         {
             m_ServiceProviderMock = new Mock<IServiceProvider>(MockBehavior.Strict);
             m_SourceModelsServiceMock = new Mock<ISourceModelsService>(MockBehavior.Strict);
+            m_EventSelectionServiceMock = new Mock<IEventSelectionService>(MockBehavior.Strict);
             m_SourceModel = new SourceModel(typeof(TestSource));
+            m_ProjectedSourceModel = new SourceModel(typeof(ProjectedTestSource));
             m_SourceModelEventField = m_SourceModel.GetOrCreateEventField(nameof(TestSource.TestEvent));
             m_PipelineMock = new Mock<IPipeline>(MockBehavior.Strict);
 
@@ -49,23 +56,9 @@ namespace FluentEvents.UnitTests.Pipelines.Projections
             [Values] bool isEventFieldNameNull
         )
         {
-            m_ServiceProviderMock
-                .Setup(x => x.GetService(typeof(ISourceModelsService)))
-                .Returns(m_SourceModelsServiceMock.Object)
-                .Verifiable();
-
-            var projectedSourceModel = new SourceModel(typeof(ProjectedTestSource));
-
-            m_SourceModelsServiceMock
-                .Setup(x => x.GetOrCreateSourceModel(typeof(ProjectedTestSource)))
-                .Returns(projectedSourceModel)
-                .Verifiable();
-
-            m_PipelineMock
-                .Setup(x =>
-                    x.AddModule<ProjectionPipelineModule, ProjectionPipelineModuleConfig>(
-                        It.IsAny<ProjectionPipelineModuleConfig>()))
-                .Verifiable();
+            SetUpServiceProvider();
+            SetUpSourceModelsService();
+            SetUpPipeline();
 
             var newEventPipelineConfigurator = m_EventPipelineConfigurator.ThenIsProjected(
                 x => new ProjectedTestSource(),
@@ -73,30 +66,42 @@ namespace FluentEvents.UnitTests.Pipelines.Projections
                 isEventFieldNameNull ? null : nameof(ProjectedTestSource.TestEvent2)
             );
 
-            Assert.That(
-                newEventPipelineConfigurator,
-                Is.TypeOf<EventPipelineConfigurator<ProjectedTestSource, ProjectedTestEventArgs>>()
-            );
-            Assert.That(newEventPipelineConfigurator.Get<SourceModel>(), Is.EqualTo(projectedSourceModel));
-            Assert.That(newEventPipelineConfigurator.Get<IServiceProvider>(), Is.EqualTo(m_ServiceProviderMock.Object));
-            Assert.That(newEventPipelineConfigurator.Get<IPipeline>(), Is.EqualTo(m_PipelineMock.Object));
+            AssertThatPipelineModuleIsAdded(newEventPipelineConfigurator);
         }
 
+        [Test]
+        public void ThenIsProjected_WithSelectionAction_ShouldAddPipelineModule()
+        {
+            SetUpServiceProvider();
+            SetUpSourceModelsService();
+            SetUpPipeline();
+
+            m_ServiceProviderMock
+                .Setup(x => x.GetService(typeof(IEventSelectionService)))
+                .Returns(m_EventSelectionServiceMock.Object)
+                .Verifiable();
+            
+            Action<ProjectedTestSource, dynamic> selectionAction = (x, y) => { };
+
+            m_EventSelectionServiceMock
+                .Setup(x => x.GetSelectedEvent(m_ProjectedSourceModel, selectionAction))
+                .Returns(new [] { nameof(ProjectedTestSource.TestEvent2) })
+                .Verifiable();
+
+            var newEventPipelineConfigurator = m_EventPipelineConfigurator.ThenIsProjected(
+                x => new ProjectedTestSource(),
+                x => new ProjectedTestEventArgs(),
+                selectionAction
+            );
+
+            AssertThatPipelineModuleIsAdded(newEventPipelineConfigurator);
+        }
 
         [Test]
         public void ThenIsProjected_WithInvalidEventFieldName_ShouldThrow()
         {
-            m_ServiceProviderMock
-                .Setup(x => x.GetService(typeof(ISourceModelsService)))
-                .Returns(m_SourceModelsServiceMock.Object)
-                .Verifiable();
-
-            var projectedSourceModel = new SourceModel(typeof(ProjectedTestSource));
-
-            m_SourceModelsServiceMock
-                .Setup(x => x.GetOrCreateSourceModel(typeof(ProjectedTestSource)))
-                .Returns(projectedSourceModel)
-                .Verifiable();
+            SetUpServiceProvider();
+            SetUpSourceModelsService();
 
             Assert.That(() =>
             {
@@ -122,6 +127,46 @@ namespace FluentEvents.UnitTests.Pipelines.Projections
                     isEventArgsConverterNull ? (Func<TestEventArgs, ProjectedTestEventArgs>)null : x => new ProjectedTestEventArgs()
                 );
             }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        private void SetUpServiceProvider()
+        {
+            m_ServiceProviderMock
+                .Setup(x => x.GetService(typeof(ISourceModelsService)))
+                .Returns(m_SourceModelsServiceMock.Object)
+                .Verifiable();
+        }
+
+        private void AssertThatPipelineModuleIsAdded(
+            EventPipelineConfigurator<ProjectedTestSource, ProjectedTestEventArgs> newEventPipelineConfigurator
+        )
+        {
+            Assert.That(
+                newEventPipelineConfigurator,
+                Is.TypeOf<EventPipelineConfigurator<ProjectedTestSource, ProjectedTestEventArgs>>()
+            );
+            Assert.That(newEventPipelineConfigurator.Get<SourceModel>(), Is.EqualTo(m_ProjectedSourceModel));
+            Assert.That(newEventPipelineConfigurator.Get<IServiceProvider>(), Is.EqualTo(m_ServiceProviderMock.Object));
+            Assert.That(newEventPipelineConfigurator.Get<IPipeline>(), Is.EqualTo(m_PipelineMock.Object));
+        }
+
+        private void SetUpPipeline()
+        {
+            m_PipelineMock
+                .Setup(x =>
+                    x.AddModule<ProjectionPipelineModule, ProjectionPipelineModuleConfig>(
+                        It.IsAny<ProjectionPipelineModuleConfig>()
+                    )
+                )
+                .Verifiable();
+        }
+
+        private void SetUpSourceModelsService()
+        {
+            m_SourceModelsServiceMock
+                .Setup(x => x.GetOrCreateSourceModel(typeof(ProjectedTestSource)))
+                .Returns(m_ProjectedSourceModel)
+                .Verifiable();
         }
 
         private class TestSource
