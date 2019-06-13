e4e9fe6856e92252e2d25d0cff3b8690d933adc4
luca-esse <luca-esse@users.noreply.github.com>
2019-06-13 18:13:44 +0200

    Fix wrong reference

--- src/FluentEvents/Subscriptions/ISubscriptionsCancellationToken.cs
+++ src/FluentEvents/Subscriptions/ISubscriptionsCancellationToken.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     ///     A token that can be used to cancel a global subscription with the
-    ///     <see cref="EventsContext.CancelGlobalSubscriptions(ISubscriptionsCancellationToken)"/> method.
+    ///     <see cref="EventsContext.Unsubscribe(ISubscriptionsCancellationToken)"/> method.
     /// </summary>
     public interface ISubscriptionsCancellationToken
     {
