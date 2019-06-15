bf1bae045151a21437840b55f0f8069a8815df46
luca-esse <luca-esse@users.noreply.github.com>
2019-06-15 09:45:05 +0200

    Fix SubscriptionsBuilder tests

--- src/FluentEvents.UnitTests/Config/SubscriptionsBuilderTests.cs
+++ src/FluentEvents.UnitTests/Config/SubscriptionsBuilderTests.cs
@@ -12,6 +12,7 @@ namespace FluentEvents.UnitTests.Config
     {
         private Mock<IGlobalSubscriptionsService> _globalSubscriptionsService;
         private Mock<IScopedSubscriptionsService> _scopedSubscriptionsService;
+        private Mock<IEventSelectionService> _eventSelectionService;
         private Mock<ISourceModelsService> _sourceModelsService;
 
         private SubscriptionsBuilder _subscriptionsBuilder;
@@ -21,13 +22,24 @@ namespace FluentEvents.UnitTests.Config
         {
             _globalSubscriptionsService = new Mock<IGlobalSubscriptionsService>(MockBehavior.Strict);
             _scopedSubscriptionsService = new Mock<IScopedSubscriptionsService>(MockBehavior.Strict);
+            _eventSelectionService = new Mock<IEventSelectionService>(MockBehavior.Strict);
             _sourceModelsService = new Mock<ISourceModelsService>(MockBehavior.Strict);
 
             _subscriptionsBuilder = new SubscriptionsBuilder(
                 _globalSubscriptionsService.Object,
                 _scopedSubscriptionsService.Object,
-                _sourceModelsService.Object
+                _sourceModelsService.Object,
+                _eventSelectionService.Object
             );
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _globalSubscriptionsService.Verify();
+            _scopedSubscriptionsService.Verify();
+            _eventSelectionService.Verify();
+            _sourceModelsService.Verify();
         }
 
         [Test]
@@ -42,6 +54,11 @@ namespace FluentEvents.UnitTests.Config
         [Test]
         public void ServiceHandler_ShouldReturnServiceHandlerConfigurator()
         {
+            _sourceModelsService
+                .Setup(x => x.GetOrCreateSourceModel(typeof(object)))
+                .Returns(new SourceModel(typeof(object)))
+                .Verifiable();
+
             var serviceConfigurator = _subscriptionsBuilder.ServiceHandler<SubscribingService, object, object>();
 
             Assert.That(serviceConfigurator, Is.Not.Null);
