55554f0e9ac9e13254860ded3a121827e788a7ba
luca-esse <luca-esse@users.noreply.github.com>
2019-07-12 23:37:03 +0200

    Add AsyncDomainEventHandler

---
+++ src/FluentEvents/AsyncDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace FluentEvents
+{
+    /// <summary>
+    ///     Represents the method that will handle a domain event.
+    /// </summary>
+    /// <param name="domainEvent">The event.</param>
+    /// <typeparam name="TDomainEvent">The type of the event.</typeparam>
+    public delegate Task AsyncDomainEventHandler<in TDomainEvent>(TDomainEvent domainEvent) where TDomainEvent : class;
+}
