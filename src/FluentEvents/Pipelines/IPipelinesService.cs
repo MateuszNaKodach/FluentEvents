f676591a75770c3727b74cf98f0c3613386a97f0
luca-esse <luca-esse@users.noreply.github.com>
2019-07-16 19:16:03 +0200

    Move IPipelinesService to own file

---
+++ src/FluentEvents/Pipelines/IPipelinesService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluentEvents.Pipelines
+{
+    /// <summary>
+    ///     This API supports the FluentEvents infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public interface IPipelinesService
+    {
+        /// <summary>
+        ///     This API supports the FluentEvents infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        void AddPipeline(Type eventType, IPipeline pipeline);
+
+        /// <summary>
+        ///     This API supports the FluentEvents infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        IEnumerable<IPipeline> GetPipelines(Type eventType);
+    }
+}
--- src/FluentEvents/Pipelines/PipelinesService.cs
+++ src/FluentEvents/Pipelines/PipelinesService.cs
@@ -9,25 +9,6 @@ namespace FluentEvents.Pipelines
     ///     This API supports the FluentEvents infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    public interface IPipelinesService
-    {
-        /// <summary>
-        ///     This API supports the FluentEvents infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        void AddPipeline(Type eventType, IPipeline pipeline);
-
-        /// <summary>
-        ///     This API supports the FluentEvents infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        IEnumerable<IPipeline> GetPipelines(Type eventType);
-    }
-
-    /// <summary>
-    ///     This API supports the FluentEvents infrastructure and is not intended to be used
-    ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
     public class PipelinesService : IPipelinesService
     {
         private readonly ConcurrentDictionary<Type, ConcurrentStack<IPipeline>> _pipelines;
