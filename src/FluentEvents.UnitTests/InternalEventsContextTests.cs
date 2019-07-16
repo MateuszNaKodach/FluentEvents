13c7bc2f91d77eb96c87cffa7f35f0770d75a927
luca-esse <luca-esse@users.noreply.github.com>
2019-07-16 21:31:31 +0200

    Add InternalEventsContextTests tests

---
+++ src/FluentEvents.UnitTests/InternalEventsContextTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FluentEvents.Config;
+using FluentEvents.Infrastructure;
+using Moq;
+using NUnit.Framework;
+
+namespace FluentEvents.UnitTests
+{
+    [TestFixture]
+    public class InternalEventsContextTests
+    {
+        private EventsContextOptions _options;
+        private Mock<IAppServiceProvider> _appServiceProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _options = new EventsContextOptions();
+            _appServiceProvider = new Mock<IAppServiceProvider>(MockBehavior.Strict);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _appServiceProvider.Verify();
+        }
+        
+        [Test]
+        public void Ctor_ShouldCallActionsAndCreateInternalServiceProvider()
+        {
+            var isOnConfiguringInvoked = false;
+            var isOnBuildingPipelinesInvoked = false;
+            var isOnBuildingSubscriptionsInvoked = false;
+
+            Action<EventsContextOptions> onConfiguring = x => { isOnConfiguringInvoked = true; };
+            Action<PipelinesBuilder> onBuildingPipelines = x => { isOnBuildingPipelinesInvoked = true; };
+            Action<SubscriptionsBuilder> onBuildingSubscriptions = x => { isOnBuildingSubscriptionsInvoked = true; };
+
+            var internalEventsContext = new InternalEventsContext(
+                _options,
+                onConfiguring,
+                onBuildingPipelines,
+                onBuildingSubscriptions,
+                _appServiceProvider.Object
+            );
+
+            Assert.That(isOnConfiguringInvoked, Is.True);
+            Assert.That(isOnBuildingPipelinesInvoked, Is.True);
+            Assert.That(isOnBuildingSubscriptionsInvoked, Is.True);
+
+            Assert.That(internalEventsContext, Has.Property(nameof(internalEventsContext.InternalServiceProvider)).Not.Null);
+        }
+    }
+}
