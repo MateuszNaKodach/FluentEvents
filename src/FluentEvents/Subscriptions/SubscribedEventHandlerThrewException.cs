6b8afb021255c512d2de62af635af1be6a357f34
luca-esse <luca-esse@users.noreply.github.com>
2019-07-29 13:44:09 +0200

    Move SubscribedEventHandlerThrewException to it's own file

---
+++ src/FluentEvents/Subscriptions/SubscribedEventHandlerThrewException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace FluentEvents.Subscriptions
+{
+    [Serializable]
+    internal class SubscribedEventHandlerThrewException : FluentEventsException
+    {
+        public SubscribedEventHandlerThrewException(TargetInvocationException targetInvocationException) 
+            : base("The event handler threw an exception.", targetInvocationException.InnerException)
+        {
+        }
+    }
+}
