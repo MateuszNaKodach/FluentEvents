e53684e1c205ee9db5c6a1ccf536ed46ae2dbe67
luca-esse <luca-esse@users.noreply.github.com>
2019-08-02 21:57:21 +0200

    Make EventTypeMismatchException internal

--- src/FluentEvents/Subscriptions/EventTypeMismatchException.cs
+++ src/FluentEvents/Subscriptions/EventTypeMismatchException.cs
@@ -2,12 +2,8 @@
 
 namespace FluentEvents.Subscriptions
 {
-    /// <summary>
-    ///     An exception that is thrown when the source of the event to publish
-    ///     is not an instance of <see cref="Subscription.EventType"/>.
-    /// </summary>
     [Serializable]
-    public class EventTypeMismatchException : FluentEventsException
+    internal class EventTypeMismatchException : FluentEventsException
     {
         internal EventTypeMismatchException() 
             : base($"The event source type doesn't match the {nameof(Subscription)}.{nameof(Subscription.EventType)}")
